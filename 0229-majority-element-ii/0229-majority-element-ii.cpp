29c6fcd990ea39756a399c96e590723406a510bc
Aniket Vishwakarma <aniketjangid05@gmail.com>
2026-09-09 22:11:10 +0530

    Time: 0 ms (100%), Space: 23.7 MB (87.35%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -1,36 +1,65 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-        int cnt1 = 0,cnt2 =0;
-        int el1 = INT_MIN;
-        int el2 = INT_MIN;
-        for(int i = 0;i<nums.size();i++){
-            if(cnt1==0 && el2!= nums[i]){
-                cnt1 = 1;
+
+        int el1 = 0;
+        int el2 = 1;
+
+        int cnt1 = 0;
+        int cnt2 = 0;
+
+        // Phase 1: Find potential candidates
+
+        for(int i = 0; i < nums.size(); i++){
+
+            if(nums[i] == el1){
+                cnt1++;
+            }
+
+            else if(nums[i] == el2){
+                cnt2++;
+            }
+
+            else if(cnt1 == 0){
                 el1 = nums[i];
+                cnt1 = 1;
             }
-            else if(cnt2==0&&el1!=nums[i]){
-                cnt2=1;
-                el2=nums[i];
 
+            else if(cnt2 == 0){
+                el2 = nums[i];
+                cnt2 = 1;
             }
-            else if(nums[i]==el1)cnt1++;
-            else if(nums[i]==el2)cnt2++;
-            else {
-                cnt1--,cnt2--;
+
+            else{
+                cnt1--;
+                cnt2--;
             }
         }
-        vector<int> ls;
-        cnt1=0,cnt2=0;
-        for(int i = 0;i<nums.size();i++){
-            if(el1==nums[i])cnt1++;
-            if(el2==nums[i])cnt2++;
 
+        // Phase 2: Count actual frequency
+
+        int count1 = 0;
+        int count2 = 0;
+
+        for(int i = 0; i < nums.size(); i++){
+
+            if(nums[i] == el1)
+                count1++;
+
+            else if(nums[i] == el2)
+                count2++;
         }
-        int mini = (int)(nums.size()/3)+1;
-        if(cnt1>=mini) ls.push_back(el1);
-        if(cnt2>=mini) ls.push_back(el2);
-        sort(ls.begin(),ls.end());
-        return ls;
+
+        // Phase 3: Verify
+
+        vector<int> ans;
+
+        if(count1 > nums.size() / 3)
+            ans.push_back(el1);
+
+        if(count2 > nums.size() / 3)
+            ans.push_back(el2);
+
+        return ans;
     }
 };
